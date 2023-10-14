72b789e136bcc1e3f7c340fbd05dc9e2fa97a71f
Marcos Aurelio <marcosaureliosl@gmail.com>
2023-10-14 16:22:59 -0300

    js aulas

---
+++ projetos das aulas aldeota/aula-operações-js/operações.js
@@ -0,0 +1,83 @@
+// // soma em js
+
+// let soma = 25 + 25 + 25
+
+// console.log(soma)
+
+// // subtração em js
+
+// let sub = 50 - 35
+
+// console.log(sub)
+
+// // multiplicação
+
+// let mult = 30 * 3
+// console.log(mult)
+
+// let idade = "42"
+
+// console.log(+idade + 1 )
+
+
+
+// let idade = prompt ( "qual a sua idade ?")
+// alert(+ idade + 1)
+
+// console.log(+idade+1)
+
+
+// let idade = prompt ( "qual a sua idade ?")
+// alert( idade -10)
+
+// console.log(idade-1)
+
+
+// alert((10+10)*2)
+
+// crie uma expressão que de resultado NAN
+// let soma = "10s" / 2
+
+// console.log(soma)
+
+// let soma1 = "10"
+// let soma2 = "10"
+
+// alert(soma1+soma2)
+
+// let idade = prompt ( "qual a sua idade ? irei adicionar 10")
+// alert(+ idade + 10)
+
+// console.log(+idade+10)
+
+// let incremento = 60
+// console.log(incremento++);
+// console.log(incremento++);
+// console.log(incremento);
+
+// let incremento = 49
+// console.log(++incremento);
+// console.log(incremento);
+
+
+
+
+
+
+
+// Qual o resultado da seguinte expressão?
+// var total = 40 + 5 * 2 / 2 + 10;
+
+// Crie uma expressão que retorne NaN
+
+// Somar a string '10' com o número 10 e retornar 1010
+
+// Incremente o número 5 e retorne o seu valor incrementado
+
+// Como dividir o peso por 2?
+// let numero = '90';
+// let unidade = 'kg';
+// let peso = numero + unidade; // '90kg'
+// let pesoPorDois = (+numero) / 2; // NaN (Not a Number)
+
+// alert(pesoPorDois + unidade)
---
+++ projetos das aulas aldeota/aulas-js/scripty.js
@@ -0,0 +1,15 @@
+// aulas de js - declaração de variavel 
+
+let  nome = "Marcos"
+let sobreNome = " Aurelio, "
+let idade = 39 
+let ano = " anos!" 
+
+let frase = " e o pix, nada ainda ? 👍"
+// prompt (frase)
+
+alert (typeof nome)
+
+prompt (nome + sobreNome + idade + ano + frase)
+
+
