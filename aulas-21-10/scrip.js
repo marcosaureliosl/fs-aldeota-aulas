04f6680d781ccdc5e803aa1fe0a9eac2700f5d6a
Marcos Aurelio <marcosaureliosl@gmail.com>
2023-10-21 16:54:18 -0300

    aulas js-condicional 2

---
+++ aulas-21-10/scrip.js
@@ -0,0 +1,48 @@
+
+// let resposta = prompt("Voce é maior de 17 anos?");
+
+
+// if(resposta>17) {
+
+// alert("pode dirigir")
+
+// } else{
+//     alert("não pode dirigir")
+// } 
+
+// if ("") {
+// alert("verdadeiro")
+// } else {
+// alert("falso")
+// }
+
+// let semaforo = 'verde'
+
+// switch (semaforo) {
+//     case "amarelo" :
+//         alert("amarelo");
+//         break;
+//         case "verde" :
+//             alert("verde");
+//             break;
+//             case "vermelho" :
+//                 alert("vermelho");
+//                 break;
+// }
+
+// let resposta = prompt("Qual a sua idade?");
+
+// idade = parseInt(resposta)
+
+// if(resposta < 18) {
+
+// alert("Você é adolescente");
+
+// } else if (+resposta ===18) {
+//     alert("voçê tem 18 anos");
+// } else { 
+//     alert("você é adulto");
+// }
+
+
+
