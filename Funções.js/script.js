527fa76c039433fa5f5d6acd0ba4261162ee0c9d
Marcos Aurelio <marcosaureliosl@gmail.com>
2023-11-04 16:26:24 -0300

    aulas js

---
+++ Funções.js/script.js
@@ -0,0 +1,54 @@
+function pegarNome( ){
+    let nome = prompt("qual seu nome");
+    alert(nome);
+
+    
+}
+
+function chamarNome(){
+    let meunome = ("Marcos Aulreio");
+    alert(meunome);
+
+
+}
+
+// function pi(){
+
+//     return 3.14;
+
+// }
+// let retorno = pi() * 2
+
+// alert (retorno)
+
+// function quadrado(lado){
+//     alert(lado);
+//     return lado * lado;
+// }
+
+// let retorno = quadrado(2020);
+
+// alert(retorno)
+
+// function mult5(mult) {
+//     return mult * 5
+
+// }
+
+// let retorno=mult5(50);
+// console.log(retorno);
+// alert(retorno);
+
+function soma(numero1,numero02){
+    return numero1 + numero02;
+}
+let retorno = (soma(6,5))
+alert (retorno)
+
+
+
+
+
+
+
+
