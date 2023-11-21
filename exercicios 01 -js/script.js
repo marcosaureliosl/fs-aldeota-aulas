3ed81d2dd3001ddda3e601918399b2088ac24c3d
Marcos Aurelio <marcosaureliosl@gmail.com>
2023-11-20 22:41:18 -0300

    aulas js

---
+++ exercicios 01 -js/script.js
@@ -0,0 +1,5 @@
+function exibirMensagemBoasVindas() {
+    alert('Bem-vindo à nossa página! Esperamos que você tenha uma ótima experiência.');
+}
+
+window.addEventListener('load', exibirMensagemBoasVindas);
