7436957889f24cce40eb43eab3c1aab34ec06723
Marcos Aurelio <marcosaureliosl@gmail.com>
2023-11-20 22:49:53 -0300

    aulas js

--- exercicios 01 -js/script.js
+++ exercicios 01 -js/script.js
@@ -1,5 +1,22 @@
-function exibirMensagemBoasVindas() {
-    alert('Bem-vindo à nossa página! Esperamos que você tenha uma ótima experiência.');
+// function exibirMensagemBoasVindas() {
+//     alert('Bem-vindo à nossa página! Esperamos que você tenha uma ótima experiência.');
+// }
+
+// window.addEventListener('load', exibirMensagemBoasVindas);
+
+ // Função para verificar se um número é par ou ímpar
+ function verificarParOuImpar(numero) {
+    return numero % 2 === 0;
 }
 
-window.addEventListener('load', exibirMensagemBoasVindas);
+// Solicitar ao usuário para inserir um número
+var numeroUsuario = prompt('Digite um número:');
+
+// Verificar se o número é par ou ímpar e exibir um alerta
+var resultado = verificarParOuImpar(parseInt(numeroUsuario, 10));
+
+if (resultado) {
+    alert('O número é par!');
+} else {
+    alert('O número é ímpar!');
+}
