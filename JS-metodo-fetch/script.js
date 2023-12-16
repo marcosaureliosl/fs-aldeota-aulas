aee5e4a615788fe1ec8b09b1aad5d8d972494b01
Marcos Aurelio <marcosaureliosl@gmail.com>
2023-12-16 16:33:01 -0300

    aulas js

--- JS-metodo-fetch/script.js
+++ JS-metodo-fetch/script.js
@@ -57,6 +57,35 @@
 
 
 
+// const url =
+//   "https://servicodados.ibge.gov.br/api/v1/localidades/estados/ce/municipios";
+
+// const cidades = document.getElementById("cidades");
+
+// function criarEAddElemento(cidade) {
+//   const cidadeOption = document.createElement("option");
+//   cidadeOption.textContent = cidade.nome;
+
+//   cidades.appendChild(cidadeOption);
+// }
+
+// function buscarCidadesCE() {
+//   //retorna uma Promisse
+//   const retorno = fetch(url);
+
+//   // Then trata a Promisse
+//   retorno
+//     .then((resposta) => {
+//       // transforma a resposta em json, retorna uma nova Promisse
+//       return resposta.json();
+//     })
+//     .then((json) => {
+//       json.forEach(criarEAddElemento);
+//     });
+// }
+
+// buscarCidadesCE();
+
 const url =
   "https://servicodados.ibge.gov.br/api/v1/localidades/estados/ce/municipios";
 
@@ -69,22 +98,20 @@ function criarEAddElemento(cidade) {
   cidades.appendChild(cidadeOption);
 }
 
-function buscarCidadesCE() {
-  //retorna uma Promisse
-  const retorno = fetch(url);
+async function buscarCidadesCE() {
 
-  // Then trata a Promisse
-  retorno
-    .then((resposta) => {
-      // transforma a resposta em json, retorna uma nova Promisse
-      return resposta.json();
-    })
-    .then((json) => {
-      json.forEach(criarEAddElemento);
-    });
+try {
+
+    const resposta = await fetch(url);
+    const json = await resposta.json();
+  
+    json.forEach(criarEAddElemento);
+
+} catch (err) {
+    alert("Error na pagina",)
 }
 
+  }
+
 buscarCidadesCE();
-
-
 
