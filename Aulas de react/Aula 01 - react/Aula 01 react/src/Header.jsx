a35e7fd53b0c3dc2a933e6cb7ef508fff136cf50
Marcos Aurelio <marcosaureliosl@gmail.com>
2024-01-07 17:51:47 -0300

    aulas React

---
+++ Aulas de react/Aula 01 - react/Aula 01 react/src/Header.jsx
@@ -0,0 +1,10 @@
+function Header () {
+    return( <header>
+        <nav>
+            <a href=""></a>
+            <h1>Marcos Aurelio</h1>
+        </nav>
+    </header>)
+}
+
+export default Header;
---
+++ Aulas de react/Aula 01 - react/Aula 01 react/src/Perfil.jsx
@@ -0,0 +1,10 @@
+function Perfil () {
+    return (
+    <div>
+        <h1>Marcos Aurelio</h1>
+        <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fs2-techtudo.glbimg.com%2FL9wb1xt7tjjL-Ocvos-Ju0tVmfc%3D%2F0x0%3A1200x800%2F984x0%2Fsmart%2Ffilters%3Astrip_icc()%2Fi.s3.glbimg.com%2Fv1%2FAUTH_08fbf48bc0524877943fe86e43087e7a%2Finternal_photos%2Fbs%2F2023%2Fq%2Fl%2FTIdfl2SA6J16XZAy56Mw%2Fcanvaai.png&tbnid=a_sljx2cUPsCEM&vet=12ahUKEwi58IG4xMmDAxWoNLkGHaa3Bu8QMygBegQIARBw..i&imgrefurl=https%3A%2F%2Fwww.techtudo.com.br%2Flistas%2F2023%2F08%2Finteligencia-artificial-que-cria-imagens-com-palavras-veja-8-opcoes-gratis-edsoftwares.ghtml&docid=VR86AiCted83bM&w=984&h=656&q=imagem&ved=2ahUKEwi58IG4xMmDAxWoNLkGHaa3Bu8QMygBegQIARBw" alt="img panda" />
+    </div>
+    );
+    
+}
+export default Perfil;
