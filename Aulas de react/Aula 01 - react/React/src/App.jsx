1b9eeed50be8818755980e43f79a10de0d115a41
Marcos Aurelio <marcosaureliosl@gmail.com>
2024-01-07 18:36:46 -0300

    aulas React

---
+++ Aulas de react/Aula 01 - react/React/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Header from "./Header";
+import Perfil from "./Perfil";
+
+
+function App() {
+    return (
+    <div> 
+    <Header />
+    <Perfil />
+    </div>
+    );
+}
+
+export default App;
