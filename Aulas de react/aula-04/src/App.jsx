986b1e69ef9218630ad06ac0fd6c3a5759d91c4a
Marcos Aurelio <marcosaureliosl@gmail.com>
2024-01-27 18:01:06 -0300

    aula react

--- Aulas de react/aula-04/src/App.jsx
+++ Aulas de react/aula-04/src/App.jsx
@@ -1,42 +1,58 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+
+// Função separada para obter dados da API
+async function fetchData(url) {
+  const resposta = await fetch(url);
+
+  if (!resposta.ok) {
+    throw new Error(`Falha ao obter dados da API: ${resposta.statusText}`);
+  }
+
+  return resposta.json();
+}
 
 function App() {
   const [cidades, setCidades] = useState([]);
   const [estados, setEstados] = useState([]);
   const [estadoSelecionado, setEstadoSelecionado] = useState('');
 
-  async function getCidades(uf) {
-    const resposta = await fetch(
-      `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`
-    );
-
-    const dados = await resposta.json();
-
-    setCidades(dados);
+  // Função para obter cidades com base no estado selecionado
+  async function obterCidades(uf) {
+    try {
+      const dados = await fetchData(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`);
+      setCidades(dados);
+    } catch (error) {
+      console.error(`Erro ao obter cidades: ${error.message}`);
+    }
   }
 
-  async function getEstados() {
-    const resposta = await fetch(
-      "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
-    );
-
-    const dados = await resposta.json();
-
-    setEstados(dados);
+  // Função para obter todos os estados
+  async function obterEstados() {
+    try {
+      const dados = await fetchData("https://servicodados.ibge.gov.br/api/v1/localidades/estados");
+      setEstados(dados);
+    } catch (error) {
+      console.error(`Erro ao obter estados: ${error.message}`);
+    }
   }
 
   useEffect(() => {
-    getEstados();
+    obterEstados();
   }, []);
 
+  // Manipulador de mudança de estado
   const handleEstadoChange = (event) => {
     const ufSelecionado = event.target.value;
     setEstadoSelecionado(ufSelecionado);
-    getCidades(ufSelecionado);
+    obterCidades(ufSelecionado);
   };
 
-  const estilo = { border: "1px solid #fff",borderRadius: "8px" , padding: "0.6em 1.2em" };
+  const estilo = {
+    border: "1px solid #fff",
+    borderRadius: "8px",
+    padding: "0.6em 1.2em"
+  };
 
   return (
     <>
