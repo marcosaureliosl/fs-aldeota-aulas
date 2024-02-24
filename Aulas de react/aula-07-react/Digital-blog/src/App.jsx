dc3390e7cbfb51ede0511893b3c59779efa48002
Marcos Aurelio <marcosaureliosl@gmail.com>
2024-02-24 18:42:31 -0300

    aulas react

--- Aulas de react/aula-07-react/Digital-blog/src/App.jsx
+++ Aulas de react/aula-07-react/Digital-blog/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Users from "./pages/Users";
 import Post from "./Post";
+import NotFound from "./pages/Notfound"; 
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <Route path="/home" element={<Home />} />
             <Route path="/users" element={<Users />} />
             <Route path="/posts/:id" element={<Post />} />
+            <Route path="*" element={<NotFound />} /> {/* Use NotFound aqui */}
         </Routes>
     );
 };
